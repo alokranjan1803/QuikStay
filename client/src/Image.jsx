aa39033367a9d6a5919c327cb44de878c04c6588
Alok Ranjan <119922910+alokranjan1803@users.noreply.github.com>
2024-08-22 19:30:32 +0530

    Update Image.jsx

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -1,7 +1,7 @@
 export default function Image({src,...rest}) {
     src = src && src.includes('https://')
       ? src
-      : 'http://localhost:5000/uploads/'+src;
+      : 'hotel-mingle-api.vercel.app/uploads/'+src;
     return (
       <img {...rest} src={src} alt={''} />
     );
