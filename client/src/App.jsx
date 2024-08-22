c660f06fefeb7c8cedbaf2881f1bd796b97d4ad8
Alok Ranjan <119922910+alokranjan1803@users.noreply.github.com>
2024-08-22 19:47:59 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ import BookingsPage from "./pages/BookingsPage";
 import BookingPage from "./pages/BookingPage";
 
 
-axios.defaults.baseURL = hotel-mingle-api.vercel.app;
+axios.defaults.baseURL = ${import.meta.env.VITE_API_BASE_URL};
 axios.defaults.withCredentials = true;
 
 
