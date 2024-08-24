7d166358b4c9ecd5ba5009ec66a361275742f2c0
Alok Ranjan <119922910+alokranjan1803@users.noreply.github.com>
2024-08-24 19:39:42 +0530

    Update BookingWidget.jsx

--- client/src/BookingWidget.jsx
+++ client/src/BookingWidget.jsx
@@ -42,7 +42,7 @@ export default function BookingWidget({ place }) {
     );
     const bookingId = response.data._id;
     setRedirect(
-      `${import.meta.env.VITE_API_BASE_URL}/account/bookings/${bookingId}`
+      `/account/bookings/${bookingId}`
     );
   }
 
