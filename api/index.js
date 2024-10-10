66cde9e3f4f20299ea1aea4ab2902a2575119fc5
Alok Ranjan <119922910+alokranjan1803@users.noreply.github.com>
2024-10-10 15:49:50 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     credentials: true,
-    origin: process.env.CLIENT_API,
+    origin: "https://hotel-mingle-client.vercel.app",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   })
 );
