b92056c847d075f0e95819592acfc5fd242552f7
Alok Ranjan <119922910+alokranjan1803@users.noreply.github.com>
2024-10-11 08:33:13 +0530

    Delete api/node_modules directory

--- api/node_modules/@rive-app/react-canvas/dist/types/hooks/elementObserver.d.ts
+++
@@ -1,9 +0,0 @@
-declare class ElementObserver {
-    private observer;
-    private elementsMap;
-    constructor();
-    onObserved: (entries: IntersectionObserverEntry[]) => void;
-    registerCallback(element: Element, callback: Function): void;
-    removeCallback(element: Element): void;
-}
-export default ElementObserver;
--- api/node_modules/@rive-app/react-canvas/dist/types/hooks/useIntersectionObserver.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Hook to observe elements when they are intersecting with the viewport
- *
- * @returns - API to observer and unobserve elements
- */
-export default function useIntersectionObserver(): {
-    observe: (element: Element, callback: Function) => void;
-    unobserve: (element: Element) => void;
-};
